b2c01587412c3a505cc4b2cff47c2c9afceee9d0
QueenOfBugs <wj915614874@163.com>
2022-04-07 10:51:22 +0800

    update

---
+++ tst.py
@@ -0,0 +1,11 @@
+# f = open('file')
+# while True:
+#     a = f.readline()
+#     if a.strip():
+#         print(a)
+
+f = open('file')
+while True:
+    a = f.readline()
+    if a.strip():
+        print(a)
